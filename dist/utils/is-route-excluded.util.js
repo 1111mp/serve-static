9e0dbe6a64211061eb950798c224e856afc79ec7
The1111mp <The1111mp@outlook.com>
2025-01-18 20:46:30 +0800

    feat: support express v5
    Signed-off-by: The1111mp <The1111mp@outlook.com>

--- dist/utils/is-route-excluded.util.js
+++ dist/utils/is-route-excluded.util.js
@@ -1,18 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.isRouteExcluded = void 0;
-const pathToRegexp = require("path-to-regexp");
+const path_to_regexp_1 = require("path-to-regexp");
 const isRouteExcluded = (req, paths = []) => {
-    return paths.some(path => {
-        const re = pathToRegexp(path);
+    return paths.some((path) => {
+        const matcher = (0, path_to_regexp_1.match)(path, { decode: decodeURIComponent });
         const queryParamsIndex = req.originalUrl.indexOf('?');
         const pathname = queryParamsIndex >= 0
             ? req.originalUrl.slice(0, queryParamsIndex)
             : req.originalUrl;
-        if (!re.exec(pathname + '/')) {
-            return false;
-        }
-        return true;
+        return matcher(pathname) !== false;
     });
 };
 exports.isRouteExcluded = isRouteExcluded;
--- lib/utils/is-route-excluded.util.ts
+++ lib/utils/is-route-excluded.util.ts
@@ -1,17 +1,15 @@
-import * as pathToRegexp from 'path-to-regexp';
+import { match } from 'path-to-regexp';
 
 export const isRouteExcluded = (req: any, paths: string[] = []) => {
-  return paths.some(path => {
-    const re = pathToRegexp(path);
+  return paths.some((path) => {
+    const matcher = match(path, { decode: decodeURIComponent });
+
     const queryParamsIndex = req.originalUrl.indexOf('?');
     const pathname =
       queryParamsIndex >= 0
         ? req.originalUrl.slice(0, queryParamsIndex)
         : req.originalUrl;
 
-    if (!re.exec(pathname + '/')) {
-      return false;
-    }
-    return true;
+    return matcher(pathname) !== false;
   });
 };
